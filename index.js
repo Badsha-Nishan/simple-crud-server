f371e611ff5cf6d62465efb85ce93c2d05ca0cfe
Badsha-Nishan <badshanisan14@gmail.com>
2026-05-08 10:38:35 +0600

    added delete user data

--- index.js
+++ index.js
@@ -40,6 +40,16 @@ async function run() {
       console.log(id);
       res.send(user);
     });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
